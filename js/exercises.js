5b29060ed0ae3bcaf23f28399b70f07e17d72531
Michael Baker <michael.a.baker34@gmail.com>
2021-01-10 21:57:23 -0600

    add exercise 56 and 57

--- js/exercises.js
+++ js/exercises.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // Exercise 50
-// Write a function definition named first that takes in sequence and returns the first value of that sequence.
+// Write a function definition named first that takes in sequence
+// and returns the first value of that sequence.
 
 function first(x) {
     return x[0];
@@ -16,36 +17,54 @@ function second(x) {
 }
 
 // Exercise 52
-// Write a function definition named third that takes in sequence and returns the third value of that sequence.
+// Write a function definition named third that takes in sequence
+// and returns the third value of that sequence.
 
 function third(x) {
     return x[2];
 }
 
 // Exercise 53
-// Write a function definition named forth that takes in sequence and returns the forth value of that sequence.
+// Write a function definition named forth that takes in sequence
+// and returns the forth value of that sequence.
 
 function forth(x) {
     return x[3];
 }
 
 // Exercise 54
-// Write a function definition named last that takes in sequence and returns the last value of that sequence.
+// Write a function definition named last that takes in sequence
+// and returns the last value of that sequence.
 
 function last(x) {
     return x[x.length - 1];
 }
 
 // Exercise 55
-// Write a function definition named secondToLast that takes in sequence and returns the second to last value of that sequence.
+// Write a function definition named secondToLast that takes in sequence
+// and returns the second to last value of that sequence.
 
 function secondToLast(x) {
     return x[x.length - 2];
 }
 
 // Exercise 56
-// Write a function definition named thirdToLast that takes in sequence and returns the third to last value of that sequence.
+// Write a function definition named thirdToLast that takes in sequence
+// and returns the third to last value of that sequence.
 
 function thirdToLast(x) {
     return x[x.length - 3];
 }
+
+// Exercise 57
+// Write a function definition named firstAndSecond that takes in sequence
+// and returns the first and second value of that sequence as an array
+
+function firstAndSecond(x) {
+    return x.slice(0,2);
+}
+
+// Exercise 58
+// Write a function definition named firstAndLast that takes in sequence
+// and returns the first and last value of that sequence as an array
+
