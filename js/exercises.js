7e35e85ce564decab38b55a2a5f77192c90a7b97
Michael Baker <michael.a.baker34@gmail.com>
2021-01-17 16:08:28 -0600

    add 58-80 js exercises

--- js/exercises.js
+++ js/exercises.js
@@ -68,3 +68,107 @@ function firstAndSecond(x) {
 // Write a function definition named firstAndLast that takes in sequence
 // and returns the first and last value of that sequence as an array
 
+
+
+// Exercise 59
+// Write a function definition named firstToLast that takes in sequence and returns the sequence with the first value moved to the end of the sequence.
+
+
+
+// Exercise 60
+// Write a function definition named sumAll that takes in sequence of numbers and returns all the numbers added together.
+
+
+
+//  Exercise 61
+//  Write a function definition named mean that takes in sequence of numbers and returns the average value
+
+
+
+// Exercise 62
+// Write a function definition named median that takes in sequence of numbers and returns the average value
+
+
+
+// Exercise 63
+// Write a function definition named maxMinusMin that takes in an array of numbers and returns the difference of the maximum minus theminimum.
+
+
+
+// Exercise 64
+// Write a function definition named productOfAll that takes in sequence of numbers and returns the product of multiplying all the numbers together
+
+
+
+// Exercise 65
+// Write a function definition named getHighestNumber that takes in sequence of numbers and returns the largest number.
+
+
+
+// Exercise 66
+// Write a function definition named getSmallestNumber that takes in sequence of numbers and returns the smallest number.
+
+
+
+// Exercise 67
+// Write a function definition named onlyOddNumbers that takes in sequence of numbers and returns the odd numbers in an array.
+
+
+
+// Exercise 68
+// Write a function definition named onlyEvenNumbers that takes in sequence of numbers and returns the even numbers in an array.
+
+
+
+// Exercise 69
+// Write a function definition named onlyPositiveNumbers that takes in sequence of numbers and returns the positive numbers in an array.
+
+
+
+// Exercise 70
+// Write a function definition named onlyNegativeNumbers that takes in sequence of numbers and returns the negative numbers in an array.
+
+
+
+// Exercise 71
+// Write a function definition named hasEvens that takes in sequence of numbers and returns true if there are any even numbers in the sequence
+
+
+
+// Exercise 72
+// Write a function definition named countEvens that takes in sequence of numbers and returns the number of even numbers
+
+
+
+// Exercise 73
+// Write a function definition named hasOdds that takes in sequence of numbers and returns true if there are any odd numbers in the sequence
+
+
+
+// Exercise 74
+// Write a function definition named countOdds that takes in sequence of numbers and returns a count of the any odd numbers in the sequence
+
+
+
+// Exercise 76
+// Write a function definition named countPositives that takes in sequence of numbers and returns a count of the number of positive numbers
+
+
+
+// Exercise 77
+// Write a function definition named onlyPositiveEvens that takes in sequence of numbers and returns an array containing all the positive evens from the sequence
+
+
+
+// Exercise 78
+// Write a function definition named onlyPositiveOdds that takes in sequence of numbers and returns an array containing all the positive odd numbers from the sequence
+
+
+
+// Exercise 79
+// Write a function definition named onlyNegativeEvens that takes in sequence of numbers and returns an array containing all the negative even numbers from the sequence
+
+
+
+// Exercise 80
+// Write a function definition named onlyNegativeOdds that takes in sequence of numbers and returns an array containing all the negative odd numbers from the sequence
