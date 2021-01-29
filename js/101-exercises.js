79464a162455026e082634a9db39e78ed42278be
Michael Baker <michael.a.baker34@gmail.com>
2021-01-29 13:07:42 -0600

    add: firstandlast function to warmup complete 76-80 101-exercises

--- js/101-exercises.js
+++ js/101-exercises.js
@@ -227,22 +227,39 @@ function countOdds(x) {
 // Exercise 76
 // Write a function definition named countPositives that takes in sequence of numbers and returns a count of the number of positive numbers
 
-
+function countPositives(x) {
+    return (x.filter(a => (a > 0))).length;
+  }
 
 // Exercise 77
 // Write a function definition named onlyPositiveEvens that takes in sequence of numbers and returns an array containing all the positive evens from the sequence
 
-
+function onlyPositiveEvens(x) {
+    let positiveEvens = x.filter(a => (a > 0 && a % 2 === 0));
+    return positiveEvens;
+  }
 
 // Exercise 78
 // Write a function definition named onlyPositiveOdds that takes in sequence of numbers and returns an array containing all the positive odd numbers from the sequence
 
-
+function onlyPositiveOdds(x) {
+    let positiveOdds = x.filter(a => (a > 0 && a % 2 !== 0));
+    return positiveOdds;
+  }
 
 // Exercise 79
 // Write a function definition named onlyNegativeEvens that takes in sequence of numbers and returns an array containing all the negative even numbers from the sequence
 
-
+function onlyNegativeEvens(x) {
+    let negativeEvens = x.filter(a => (a < 0 && a % 2 === 0));
+    return negativeEvens;
+  }
 
 // Exercise 80
 // Write a function definition named onlyNegativeOdds that takes in sequence of numbers and returns an array containing all the negative odd numbers from the sequence
+
+function onlyNegativeOdds(x) {
+    let negativeOdds = x.filter(a => (a < 0 && a % 2 !== 0));
+    return negativeOdds;
+  }
+
