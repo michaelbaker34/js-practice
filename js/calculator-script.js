d7f9df3d6b88dffe33da44a9db90d20b138b0f95
Michael Baker <michael.a.baker34@gmail.com>
2021-06-18 19:52:54 -0500

    add data attributes create selectors

--- js/calculator-script.js
+++ js/calculator-script.js
@@ -1,2 +1,9 @@
 "use strict;"
 
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButtons = document.querySelectorAll('[data-equals]');
+const deleteButtons = document.querySelectorAll('[data-delete]');
+const clearButtons = document.querySelectorAll('[data-clear]');
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
