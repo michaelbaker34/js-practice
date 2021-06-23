ee9ea4f0469403e7694af42dc96074f2df8752f8
Michael Baker <michael.a.baker34@gmail.com>
2021-06-22 23:11:02 -0500

    define output operations

--- js/calculator-script.js
+++ js/calculator-script.js
@@ -5,6 +5,30 @@ class Calculator {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
     }
+
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        
+    }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]');
