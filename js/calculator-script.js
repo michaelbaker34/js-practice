5c08387243079d65b2f270191c717b5bd3f078c7
Michael Baker <michael.a.baker34@gmail.com>
2021-06-22 23:09:26 -0500

    add calculator class with operandelement constructors

--- js/calculator-script.js
+++ js/calculator-script.js
@@ -1,5 +1,12 @@
 "use strict;"
 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButtons = document.querySelectorAll('[data-equals]');
